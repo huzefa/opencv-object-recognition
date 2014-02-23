f26a87469699b1b65ef0a6cbc1276eb4aae5c786
Huzefa <hafatakd@uwaterloo.ca>
2014-02-23 17:30:30 -0500

    PR #2: Code looks a little better now

--- cv_objectmarker.cpp
+++ cv_objectmarker.cpp
@@ -2,11 +2,11 @@
 #include <opencv/cvaux.h>
 #include <opencv/highgui.h>
 
-// for filelisting
+// Filelisting
 #include <stdio.h>
 #include <sys/io.h>
 
-// for fileoutput
+// File IO
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -15,58 +15,50 @@
 
 using namespace std;
 
-IplImage* image=0;
-IplImage* image2=0;
-//int start_roi=0;
-int roi_x0=0;
-int roi_y0=0;
-int roi_x1=0;
-int roi_y1=0;
-int numOfRec=0;
+IplImage *image = 0;
+IplImage *image2 = 0;
+int roi_x0 = 0;
+int roi_y0 = 0;
+int roi_x1 = 0;
+int roi_y1 = 0;
+int numOfRec = 0;
 int startDraw = 0;
-char* window_name="<SPACE>add <B>save and load next <ESC>exit";
+char *window_name="<SPACE>add <B>save and load next <ESC>exit";
 
-string IntToString(int num)
-{
-    ostringstream myStream; //creates an ostringstream object
+string IntToString(int num) {
+    ostringstream myStream;
     myStream << num << flush;
     /*
     * outputs the number into the string stream and then flushes
     * the buffer (makes sure the output is put into the stream)
     */
-    return(myStream.str()); //returns the string form of the stringstream object
+    return(myStream.str()); // Convert to string form before returning
 };
 
-void on_mouse(int event,int x,int y,int flag, void *param)
-{
-    if(event==CV_EVENT_LBUTTONDOWN)
-    {
-        if(!startDraw)
-        {
-            roi_x0=x;
-            roi_y0=y;
+void on_mouse(int event, int x, int y, int flag, void *param) {
+    if(event == CV_EVENT_LBUTTONDOWN) {
+        if(!startDraw) {
+            roi_x0 = x;
+            roi_y0 = y;
             startDraw = 1;
         } else {
-            roi_x1=x;
-            roi_y1=y;
+            roi_x1 = x;
+            roi_y1 = y;
             startDraw = 0;
         }
     }
-    if(event==CV_EVENT_MOUSEMOVE && startDraw)
-    {
 
-        //redraw ROI selection
-        image2=cvCloneImage(image);
-        cvRectangle(image2,cvPoint(roi_x0,roi_y0),cvPoint(x,y),CV_RGB(255,0,255),1);
-        cvShowImage(window_name,image2);
+    if(event == CV_EVENT_MOUSEMOVE && startDraw) {
+        // Redraw ROI selection
+        image2 = cvCloneImage(image);
+        cvRectangle(image2, cvPoint(roi_x0, roi_y0), cvPoint(x, y),CV_RGB(255, 0, 255), 1);
+        cvShowImage(window_name, image2);
         cvReleaseImage(&image2);
     }
-
 }
 
-int main(int argc, char** argv)
-{
-    char iKey=0;
+int main(int argc, char** argv) {
+    char iKey = 0;
     string strPrefix;
     string strPostfix;
     string input_directory;
@@ -81,7 +73,7 @@ int main(int argc, char** argv)
     output_file = argv[1];
 
     /* Get a file listing of all files with in the input directory */
-    DIR    *dir_p = opendir (input_directory.c_str());
+    DIR    *dir_p = opendir(input_directory.c_str());
     struct dirent *dir_entry_p;
 
     if(dir_p == NULL) {
@@ -91,102 +83,79 @@ int main(int argc, char** argv)
 
     fprintf(stderr, "Object Marker: Input Directory: %s  Output File: %s\n", input_directory.c_str(), output_file.c_str());
 
-    //    init highgui
+    // Initialize highgui
     cvAddSearchPath(input_directory);
-    cvNamedWindow(window_name,1);
-    cvSetMouseCallback(window_name,on_mouse, NULL);
+    cvNamedWindow(window_name, 1);
+    cvSetMouseCallback(window_name, on_mouse, NULL);
 
     fprintf(stderr, "Opening directory...");
-    //    init output of rectangles to the info file
+    // Initialize output of rectangles to the info file
     ofstream output(output_file.c_str());
     fprintf(stderr, "done.\n");
 
-    while((dir_entry_p = readdir(dir_p)) != NULL)
-    {
-        numOfRec=0;
+    while((dir_entry_p = readdir(dir_p)) != NULL) {
+        numOfRec = 0;
 
-        if(strcmp(dir_entry_p->d_name, ""))
-        fprintf(stderr, "Examining file %s\n", dir_entry_p->d_name);
+        if(strcmp(dir_entry_p->d_name, "")) {
+        	fprintf(stderr, "Examining file %s\n", dir_entry_p->d_name);
+        }
 
-        /* TODO: Assign postfix/prefix info */
-        strPostfix="";
-        //strPrefix=input_directory;
-        strPrefix=dir_entry_p->d_name;
-        //strPrefix+=bmp_file.name;
+        strPostfix = "";
+        strPrefix = dir_entry_p->d_name;
+
         fprintf(stderr, "Loading image %s\n", strPrefix.c_str());
 
-        if((image=cvLoadImage(strPrefix.c_str(),1)) != 0)
-        {
-
-            //    work on current image
-            do
-
-    {
-                cvShowImage(window_name,image);
+        if((image = cvLoadImage(strPrefix.c_str(), 1)) != 0) {
+            // At current image
+            do {
+                cvShowImage(window_name, image);
 
                 // used cvWaitKey returns:
                 //    <B>=66        save added rectangles and show next image
                 //    <ESC>=27        exit program
                 //    <Space>=32        add rectangle to current image
                 //  any other key clears rectangle drawing only
-                iKey=cvWaitKey(0);
-                switch(iKey)
-                {
-
-                case 27:
-
+                iKey = cvWaitKey(0);
+                switch(iKey) {
+                	case 27:
                         cvReleaseImage(&image);
                         cvDestroyWindow(window_name);
                         return 0;
-                case 32:
-
+                	case 32:
                         numOfRec++;
-                printf("   %d. rect x=%d\ty=%d\tx2h=%d\ty2=%d\n",numOfRec,roi_x0,roi_y0,roi_x1,roi_y1);
-                //printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n",numOfRec,roi_x1,roi_y1,roi_x0-roi_x1,roi_y0-roi_y1);
-                        // currently two draw directions possible:
-                        //        from top left to bottom right or vice versa
-                        if(roi_x0<roi_x1 && roi_y0<roi_y1)
-                        {
-
-                            printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n",numOfRec,roi_x0,roi_y0,roi_x1-roi_x0,roi_y1-roi_y0);
+                		printf("   %d. rect x=%d\ty=%d\tx2h=%d\ty2=%d\n", numOfRec, roi_x0, roi_y0, roi_x1, roi_y1);
+                		//printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n",numOfRec,roi_x1,roi_y1,roi_x0-roi_x1,roi_y0-roi_y1);
+                        // Currently two draw directions possible:
+                        // from top left to bottom right or vice versa
+                        if(roi_x0 < roi_x1 && roi_y0 < roi_y1) {
+                            printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n", numOfRec, roi_x0, roi_y0, roi_x1-roi_x0, roi_y1-roi_y0);
                             // append rectangle coord to previous line content
-                            strPostfix+=" "+IntToString(roi_x0)+" "+IntToString(roi_y0)+" "+IntToString(roi_x1-roi_x0)+" "+IntToString(roi_y1-roi_y0);
+                            strPostfix += " " + IntToString(roi_x0) + " " + IntToString(roi_y0) + " " + 
+                            	IntToString(roi_x1-roi_x0) + " " + IntToString(roi_y1-roi_y0);
 
-                        }
-                        else
-                                                    //(roi_x0>roi_x1 && roi_y0>roi_y1)
-                        {
-                            printf(" hello line no 154\n");
-                            printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n",numOfRec,roi_x1,roi_y1,roi_x0-roi_x1,roi_y0-roi_y1);
+                        } else {
+                            printf("   %d. rect x=%d\ty=%d\twidth=%d\theight=%d\n", numOfRec, roi_x1, roi_y1, roi_x0-roi_x1, roi_y0-roi_y1);
                             // append rectangle coord to previous line content
-                            strPostfix+=" "+IntToString(roi_x1)+" "+IntToString(roi_y1)+" "+IntToString(roi_x0-roi_x1)+" "+IntToString      (roi_y0-roi_y1);
-        }
-
+                            strPostfix += " " + IntToString(roi_x1) + " " + IntToString(roi_y1) + " " + 
+                            	IntToString(roi_x0-roi_x1) + " " + IntToString(roi_y0-roi_y1);
+						}
                         break;
                 }
             }
-            while(iKey!=66);
-
-            {
+            while(iKey != 66);
             // save to info file as later used for HaarTraining:
             //    <rel_path>\bmp_file.name numOfRec x0 y0 width0 height0 x1 y1 width1 height1...
-            if(numOfRec>0 && iKey==66)
-            {
+            if(numOfRec > 0 && iKey == 66) {
                 //append line
                 /* TODO: Store output information. */
                 output << strPrefix << " "<< numOfRec << strPostfix <<"\n";
-
-            cvReleaseImage(&image);
+            	cvReleaseImage(&image);
             }
-
-         else 
-        {
+         } else {
             fprintf(stderr, "Failed to load image, %s\n", strPrefix.c_str());
         }
     }
-
-    }}
-
+    
     output.close();
     cvDestroyWindow(window_name);
     closedir(dir_p);
